cbe7d2a7b3ae62e3c2d48126c00e1b4e7894ca1f
bormilan <milanbor66@gmail.com>
2021-04-29 22:28:40 +0200

    packages

--- pomodoro.py
+++ pomodoro.py
@@ -13,10 +13,10 @@ def main():
     window.fill(pygame.Color('#000000'))
     pygame.display.set_caption('Pomodoro')
 
-    window.blit(pygame.image.load('munka.png'), (200,100))
-    window.blit(pygame.image.load('START.png'), (150,600))
-    window.blit(pygame.image.load('STOP.png'), (400,600))
-    window.blit(pygame.image.load('bar.png'), (60,250))
+    window.blit(pygame.image.load('pictures/munka.png'), (200,100))
+    window.blit(pygame.image.load('pictures/START.png'), (150,600))
+    window.blit(pygame.image.load('pictures/STOP.png'), (400,600))
+    window.blit(pygame.image.load('pictures/bar.png'), (60,250))
 
     pygame.display.update()
 
@@ -35,12 +35,12 @@ def main():
         if event.type == pygame.MOUSEMOTION:
             x, y = event.pos
             if x > 150 and x < 350 and y > 600 and y < 800:
-                window.blit(pygame.image.load('START_kurzor.png'), (150,600))
+                window.blit(pygame.image.load('pictures/START_kurzor.png'), (150,600))
 
         if event.type == pygame.MOUSEMOTION:
             x, y = event.pos
             if x < 150 or x > 350 or y < 600 or y > 800:
-                window.blit(pygame.image.load('START.png'), (150,600))
+                window.blit(pygame.image.load('pictures/START.png'), (150,600))
 
         #stop
         if event.type == pygame.MOUSEBUTTONDOWN:
@@ -49,8 +49,8 @@ def main():
                 if x > 400 and x < 600 and y > 600 and y < 800:
                     hanyadik = 0
                     pygame.time.set_timer(pygame.USEREVENT,0)
-                    window.blit(pygame.image.load('munka.png'), (200,100))
-                    window.blit(pygame.image.load('bar.png'), (60,250))
+                    window.blit(pygame.image.load('pictures/munka.png'), (200,100))
+                    window.blit(pygame.image.load('pictures/bar.png'), (60,250))
                     pygame.time.set_timer(pygame.USEREVENT + 1,0)
                     pygame.time.set_timer(pygame.USEREVENT + 2,0)
                     pygame.time.set_timer(pygame.USEREVENT + 3,0)
@@ -60,43 +60,43 @@ def main():
         if event.type == pygame.MOUSEMOTION:
             x, y = event.pos
             if x > 400 and x < 600 and y > 600 and y < 800:
-                window.blit(pygame.image.load('STOP_kurzor.png'), (400,600))
+                window.blit(pygame.image.load('pictures/STOP_kurzor.png'), (400,600))
 
         if event.type == pygame.MOUSEMOTION:
             x, y = event.pos
             if x < 400 or x > 600 or y < 600 or y > 800:
-                window.blit(pygame.image.load('STOP.png'), (400,600))
+                window.blit(pygame.image.load('pictures/STOP.png'), (400,600))
 
 
         if event.type == pygame.USEREVENT:           
-            window.blit(pygame.image.load('1.png'), (60,250))
+            window.blit(pygame.image.load('pictures/1.png'), (60,250))
             pygame.time.set_timer(pygame.USEREVENT + 1,szakasz)
             pygame.time.set_timer(pygame.USEREVENT,0)
 
         if event.type == pygame.USEREVENT + 1:
-            window.blit(pygame.image.load('2.png'), (60,250))
+            window.blit(pygame.image.load('pictures/2.png'), (60,250))
             pygame.time.set_timer(pygame.USEREVENT + 1,0)
             pygame.time.set_timer(pygame.USEREVENT + 2,szakasz)
 
         if event.type == pygame.USEREVENT + 2:
-            window.blit(pygame.image.load('3.png'), (60,250))
+            window.blit(pygame.image.load('pictures/3.png'), (60,250))
             pygame.time.set_timer(pygame.USEREVENT + 2, 0)
             pygame.time.set_timer(pygame.USEREVENT + 3,szakasz)
 
         if event.type == pygame.USEREVENT + 3:
-            window.blit(pygame.image.load('4.png'), (60,250))
+            window.blit(pygame.image.load('pictures/4.png'), (60,250))
             pygame.time.set_timer(pygame.USEREVENT + 3,0)
             pygame.time.set_timer(pygame.USEREVENT + 4,szakasz)
 
-            window.blit(pygame.image.load('szunet.png'), (200,100))
-            window.blit(pygame.image.load('szunetkep.png'), (60,250))
+            window.blit(pygame.image.load('pictures/szunet.png'), (200,100))
+            window.blit(pygame.image.load('pictures/szunetkep.png'), (60,250))
 
             session +=1
             print(session)
             
             #zene
             pygame.mixer.init()
-            pygame.mixer.music.load('135613__danielnieto7__alert.wav')
+            pygame.mixer.music.load('sound/135613__danielnieto7__alert.wav')
             pygame.mixer.music.play(1)
             
             hanyadik += 1
@@ -107,14 +107,14 @@ def main():
                 pygame.time.set_timer(pygame.USEREVENT + 5,szakasz)
 
         if event.type == pygame.USEREVENT + 4:
-            window.blit(pygame.image.load('munka.png'), (200,100))
-            window.blit(pygame.image.load('bar.png'), (60,250))
+            window.blit(pygame.image.load('pictures/munka.png'), (200,100))
+            window.blit(pygame.image.load('pictures/bar.png'), (60,250))
             pygame.time.set_timer(pygame.USEREVENT + 4,0)
             pygame.time.set_timer(pygame.USEREVENT,szakasz)
             
             #zene
             pygame.mixer.init()
-            pygame.mixer.music.load('135613__danielnieto7__alert.wav')
+            pygame.mixer.music.load('sound/135613__danielnieto7__alert.wav')
             pygame.mixer.music.play(1)
 
         pygame.display.update()
